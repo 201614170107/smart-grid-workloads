cd8bc0d89e14c5896647beed95d1a2e5e05364ed
Huangxin <maggiewangsjtu@gmail.com>
2015-02-28 15:47:33 -0500

    generate 5 days data

---
+++ smart-home/scripts/generateNDayTraces.py
@@ -0,0 +1,25 @@
+import os
+import numpy
+
+def generate_n_day_traces(input, output, n):
+	data = numpy.loadtxt(input, delimiter = " ")
+	newdata = []
+	for i in range(0, n, 1):
+		numdata = newdata.extend(data)
+
+	numpy.savetxt(output, newdata, fmt = '%0.5f')
+
+
+def generateAllFiles(inputDir, outputDir, n):
+	
+	for f in os.listdir(inputDir):
+		if f.endswith(".txt"):
+			generate_n_day_traces(inputDir + f, outputDir+f, n)
+
+
+if __name__ == '__main__':
+	inputDir = "..\\processedData\\microgrid\\"
+	outputDir = "..\\processedData\\microgrid_5_days\\"
+	n = 5
+	generateAllFiles(inputDir, outputDir, n)
+
