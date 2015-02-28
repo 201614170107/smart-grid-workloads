decb6580573179df4324d7cbcb1055fb0a873a42
Huangxin <maggiewangsjtu@gmail.com>
2015-02-28 15:48:44 -0500

    add comment

--- smart-home/scripts/generateNDayTraces.py
+++ smart-home/scripts/generateNDayTraces.py
@@ -1,6 +1,7 @@
 import os
 import numpy
 
+# genrate 5 days traces by repeating one day traces
 def generate_n_day_traces(input, output, n):
 	data = numpy.loadtxt(input, delimiter = " ")
 	newdata = []
@@ -10,6 +11,8 @@ def generate_n_day_traces(input, output, n):
 	numpy.savetxt(output, newdata, fmt = '%0.5f')
 
 
+# traverse all files under inputDir
+# generate 5 day traces of each file
 def generateAllFiles(inputDir, outputDir, n):
 	
 	for f in os.listdir(inputDir):
